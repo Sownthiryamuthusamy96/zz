bdf7689d1a1f14a34612ee8af3b87d6a588887a0
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-08-23 15:34:59 +0530

    Create prei1.java

---
+++ prei1.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader\u003B
+import java.io.IOException\u003B
+import java.io.InputStreamReade\u003B
+
+public class prei1 {
+	public static void main(String[] args) throws IOException
+	{
+         System.out.println("Hello World")\u003B
+		 
+	}
+}
